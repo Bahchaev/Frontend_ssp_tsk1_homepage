bb75a42be24474fcf25777e39d581bd13bf521c5
Bahchaev <wbahas@mail.ru>
2020-10-02 21:57:59 +0700

    Added images to <DeviceCard>

--- src/Components/Header/Devices/DeviceCard/index.js
+++ src/Components/Header/Devices/DeviceCard/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './styles.module.css'
+import deviceBG from './img/device.png'
+import deviceScreen from "./img/app-screen.png"
+
 
 function DeviceCard() {
     return (
-        <div className={styles.deviceCard}>
-            <div className={styles.deviceAppScreen}>
-            </div>
+        // <div className={styles.deviceCard}>
+        //     <div className={styles.deviceAppScreen}>
+        //     </div>
+        // </div>
+        <div style={{position: "relative"}}>
+            <img src={deviceBG} alt="" className={styles.deviceBG}/>
+            <img src={deviceScreen} alt="" className={styles.deviceScreen}/>
         </div>
     )
 }
