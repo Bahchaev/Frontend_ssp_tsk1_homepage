cc0c85e9d669d85efb04d1836312f62df0259b76
Bahchaev Alexandr <wbahas@mail.ru>
2020-10-06 18:39:43 +0700

    push for job location change

--- src/Components/Main/Carusel/index.js
+++ src/Components/Main/Carusel/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from "./styles.module.css"
 import caruselImg from "./img/carusel-img.png"
 import CaruselItem from "./CaruselItem";
@@ -23,24 +23,35 @@ function Carusel() {
         }
     ];
 
-    const [position, setPosition] = useState(0);
-    const handleClick = (e) => {
-        setPosition(-390 * e.currentTarget.id);
-    }
+
+
+    const [activeItem, setActiveItem] = useState(0);
+    const [position, setPosition] = useState(0)
+    const activeItemRef = useRef(null);
+    const vieFieldRef = useRef(null)
+
+    useEffect(() => {
+        setPosition(activeItem * -390 + "px")
+    }, [activeItem]);
 
     return (
         <div className={styles.caruselContainer}>
             <img src={caruselImg} alt={""} className={styles.caruselImg}/>
             <div className={styles.caruselField}>
                 <div className={styles.caruselHeader}>Trusted By More Than 55M Peoples</div>
-                <div style={{left: position + "px"}} className={styles.caruselItemsField}>
 
-                    {
-                        caruselItemsArray.map((item, index) => {
-                            return (
+                <div className={styles.caruselItemsField} ref={vieFieldRef}>
+
+                    <div className={styles.viewField} style={{left: position}}>
+                    {caruselItemsArray.map((item, index) => {
+                        const isActive = (index === activeItem);
+                        return (
+                            <div className={`styles.${isActive ? "active" : ""}`}  ref={isActive ? activeItemRef : null} id={`item-${index}`} key={`item-${index}`} >
                                 <CaruselItem index={index}/>
-                            )
-                        })}
+                            </div>
+                        )
+                    })}
+                    </div>
 
                 </div>
 
@@ -48,7 +59,7 @@ function Carusel() {
                     {caruselItemsArray.map((item, index) => {
                         return (
                             <label className={styles.caruselButton}>
-                                <input type={"radio"} name={"caruselButton"} id={index} onClick={handleClick}/>
+                                <input type={"radio"} name={"caruselButton"} id={index} onClick={() => setActiveItem(index)}/>
                                 <div/>
                             </label>
                         )
